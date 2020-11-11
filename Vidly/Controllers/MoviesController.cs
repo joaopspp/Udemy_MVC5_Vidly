cfc181b35e1fd805044d1046ff6c0a3c582772cd
joaopspp <joaopspp@gmail.com>
2020-11-11 09:43:25 -0200

    Finished first 2 sections of the course

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,7 +14,66 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-            return View();
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"},
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
+
+        // GET: Movies/movies
+        public ActionResult Movies()
+        {
+            
+            var listOfMovies = new List<Movie>()
+                {
+                    new Movie { Name = "Shrek!", Id = 1},
+                    new Movie { Name = "Wall-E", Id = 2}
+                };
+
+
+
+            var moviesViewModel = new MoviesViewModel()
+            {
+                ListOfMovies = listOfMovies
+            };
+
+
+            return View(moviesViewModel);
+        }
+
+        //Get: Movies/Customers
+        public ActionResult Customer()
+        {
+            var listOfCustomers = new List<Customer>()
+            {
+                new Customer {Name = "João", Id = 1 },
+                new Customer {Name = "Paulo", Id = 2 },
+                new Customer {Name = "Pacheco", Id = 3 }
+            };
+
+            var customerViewModel = new CustomerViewModel()
+            {
+                ListOfCustomers = listOfCustomers
+            };
+
+        return View(customerViewModel);
+        }
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
     }
 }
